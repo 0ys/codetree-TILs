5a7fa8f3bad6f9444ce65aa0857936b4a2420281
Yeseul Gong <ysgong@yonsei.ac.kr>
2024-02-01 20:51:19 +0900

    Title: 간단한 사칙연산, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240201/간단한 사칙연산/simple-arithmetic-operation.py
@@ -0,0 +1,13 @@
+'''
+a, b = 9, 4
+print(a + b, a * b, a - b, a // b, a % b, a / b, a**b)
+더하기 곱하기 빼기 나누기(몫) 나누기(나머지) 나누기(실수) 제곱
+'''
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+
+print(a+b)
+print(a-b)
+print(a//b)
+print(a%b)
