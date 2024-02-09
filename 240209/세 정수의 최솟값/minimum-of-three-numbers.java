b773612f7bd818c2f2c94db53f32aec06a0d86fa
Yeseul Gong <ysgong@yonsei.ac.kr>
2024-02-09 11:18:59 +0900

    Title: 세 정수의 최솟값, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240209/세 정수의 최솟값/minimum-of-three-numbers.java
+++ 240209/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -10,6 +10,6 @@ public class Main {
 
         if (a<=b && a<=c) System.out.println(a);
         else if (b<=a && b<=c) System.out.println(b);
-        else if (c<=a && c<=b) System.out.println(c);
+        else System.out.println(c);
     }
 }
