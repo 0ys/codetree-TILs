c2a105259775dfb21d7b3a21f097bd8de70d85cf
Yeseul Gong <ysgong@yonsei.ac.kr>
2024-02-12 00:32:34 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240212/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int sum = 0, cnt = 0;
+
+        while(true){
+            int age = sc.nextInt();
+            if (age>29) {
+                System.out.printf("%.2f", (double)sum/cnt);
+                break;
+            }
+            cnt++;
+            sum += age;
+        }
+    }
+}
